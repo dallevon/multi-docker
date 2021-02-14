ccedeed11bc1bceecf14b0565ac32357172c94c9
Levon Dallakyan <levond@starsgroup.com>
2021-02-14 13:22:58 -0500

    change client application

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Multidocker Fib calculator</h1>
           <nav>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
